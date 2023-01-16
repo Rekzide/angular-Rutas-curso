d2faa3fdd8fde6a0946a17a871e84af49fb9a106
Rekzide <b.villa@outlook.es>
2023-01-15 23:48:19 -0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,38 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InfoComponent } from './components/info/info.component';
+import { AboutComponent } from './components/about/about.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { DetalleEmpleadoComponent } from './components/detalle-empleado/detalle-empleado.component';
+import { ProyectosComponent } from './components/empleados/proyectos/proyectos.component';
+import { ExperienciaComponent } from './components/empleados/experiencia/experiencia.component';
+import { CurriculumComponent } from './components/empleados/curriculum/curriculum.component';
+import { RandomGuard } from './random.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'Informacion' },
+  { path: 'sobre', component: AboutComponent },
+  { path: 'contacto', component: ContactComponent },
+  { path: 'Informacion', component: InfoComponent, canActivate: [RandomGuard] },
+  {
+    path: 'empleados/:empleadoId', component: DetalleEmpleadoComponent, children: [
+      { path: 'proyectos', component: ProyectosComponent },
+      { path: 'experiencia', component: ExperienciaComponent },
+      { path: 'curriculum', component: CurriculumComponent }
+    ]
+  },
+  { path: '**', redirectTo: 'contacto' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+
+/**
+ * localhost:4200/empleados/122
+ * localhost:4200/empleados/456
+ * localhost:4200/empleados/324
+ */
